582e66a5333ae60290575efe5bb1edf554c79e8f
Sebastian Waterhouse <Sebastian Waterhouse>
2016-04-29 15:19:31 -0500

    ommmmmmg fixed FLASHING

--- Code/Code.ino
+++ Code/Code.ino
@@ -19,30 +19,65 @@ int paddlePlace; //Pixel X value of your paddle as read by potentiometer. Betwee
 //Ball data
 int ballSpeed = 1; //#of pixels the ball moves per second
 int bounceAngle = 270; //Temporary. Angle ball bounces off of stuff
-int bouncePercent = round(tan(bounceAngle));
-int ballRadius = 5; //Radius of ball in pixels
+int ballRadius = 6; //Radius of ball in pixels
 int ballX = 120; //Pixel X value for the ball
 int ballY = 160; // Pixel Y value for the ball
-float ballDirection = 1; //Direction ball travels, 0 being just positive Y and 90 being just positive X
-int ballPercent = round(tan(ballDirection));
+float ballInterX;
+float ballInterY;
+float ballXSpeed = 50; //Milliseconds it takes for the ball to move one x pixel
+float ballYSpeed = 70; //Milliseconds it takes for the ball to move one y pixel
 
 //Enemy paddle data
 int enemySpeed = 5; //Speed in pixels enemy paddle moves per second
 int enemyLocationX = 108; //Pixel X value for the enemy paddle
 int enemyLocationY = 32; //Pixel Y value for the enemy paddle
 
-int wall1 = 320 - (ballRadius);
-int wall2 = 0 + (ballRadius);
-int wall3 = 240 - (ballRadius);
-int wall4 = 0 + (ballRadius);
+int wall0 = 0 + (ballRadius*2);
+int wall1 = 320 - (ballRadius*2);
+int wall2 = 240 - (ballRadius*2);
 
-void doTheBall() {
-  tft.fillRect(ballX-ballRadius, ballY-ballRadius, ballRadius*2, ballRadius*2, ILI9341_BLACK);
-  tft.fillCircle(ballX, ballY, ballRadius, ILI9341_WHITE);
+void cleanUpCleanUp(){ //Everybody do your share!
+  tft.drawRect(ballX, ballY, ((ballRadius*2)+1)/**(-(int(ballXSpeed)/abs(int(ballXSpeed))))*/, ((ballRadius*2)+1)/**(-(int(ballYSpeed)/abs(int(ballYSpeed))))*/, ILI9341_BLACK);
 }
 
+void beepBoop(){ //I'm a calculator! Yaaay!
+  ballInterX = ballInterX + ballXSpeed;
+  ballInterY = ballInterY + ballYSpeed;
+
+  ballX = int(ballInterX);
+  ballY = int(ballInterY);
+}
+
+void boingyBoingy(){ //All hail Animaniacs
+  if(ballX >= wall2 || ballX <= wall0){
+    ballXSpeed = -ballXSpeed;
+  }
+  if(ballY >= wall1 || ballY <= wall0){
+    ballYSpeed = -ballYSpeed;
+  }
+}
+
+void doTheBall() {
+  boingyBoingy();
+  beepBoop();
+  tft.fillRect(ballX, ballY, ballRadius*2, ballRadius*2, ILI9341_WHITE);
+  cleanUpCleanUp();
+}
+
+/*void playerPaddleCleanUp() {
+  tft.
+}
+
+void goGoGadgetPlayerPaddle(){
+  
+}*/
+
 void setup() {
-  Serial.begin(250000);
+  ballXSpeed = (1000/ballXSpeed)/1000;
+  ballYSpeed = (1000/ballYSpeed)/1000;
+  ballInterX = ballX;
+  ballInterY = ballY;
+  Serial.begin(9600);
   Serial.println("Pong started!"); 
  
   tft.begin();
@@ -66,11 +101,5 @@ void setup() {
 
 void loop() {
   doTheBall();
-  delay(15);
-  if(ba
-  ballY = ballY + ballDirection;
-  ballX = ballX + ballDirection;
-  if(ballY <= wall2 || ballY >= wall1 || ballX <= wall4 || ballX >= wall3){
-    ballDirection = -ballDirection;
-  }
+  delay(1);
 }
